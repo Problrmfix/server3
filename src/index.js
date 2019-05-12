48065554290b9aee9bda5ddac00a04ad173d45aa
Ibrahim <39654783+Ariyanttx@users.noreply.github.com>
2019-05-12 14:59:18 +0600

    Add files via upload
    webpack working

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import "./styles.scss";
+
+console.log("hello Ibrahim!");
+console.log("hello Ibrahim!");
+console.log("hello Ibrahim!");
+console.log("hello Ibrahim!");
+console.log("hello Ibrahim!");
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
+
+
+
+
+module.exports = config;
